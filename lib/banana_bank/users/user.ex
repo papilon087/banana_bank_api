da0e1f34eb6bc3cc5805942b66b2a1c87f203f4c
papilon087 <ademirgus@gmail.com>
2023-07-24 21:28:00 -0300

    feat: encrypting user password

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -2,9 +2,15 @@ defmodule BananaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
+  # Variavél de módulo.
+  @required_params [:name, :password, :email, :cep]
+
   #Espelho para nossa tabela
   schema "users" do
     field :name, :string
+    field :password, :string, virtual: true # Campo Virtual
     field :password_hash, :string
     field :email, :string
     field :cep, :string
@@ -15,7 +21,19 @@ defmodule BananaBank.Users.User do
   # Função para mudanças no nosso banco.
   def changeset(user \\ %__MODULE__{}, params) do
     user
-    |> cast(params, [:name, :password_hash, :email, :cep])
-    |> validate_required([:name, :password_hash, :email, :cep])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
+    |> add_password_hash()
   end
+
+  # Função privada para criptografia.
+  defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  # Função privada para erro, caso não dê match.
+  defp add_password_hash(changeset), do: changeset
 end
