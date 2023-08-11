339fbf560e1dbeb8430c7cb9405b04be8b990cba
papilon087 <ademirgus@gmail.com>
2023-08-11 15:33:26 -0300

    feat: dealing with value error

--- lib/banana_bank/accounts/accounts.ex
+++ lib/banana_bank/accounts/accounts.ex
@@ -1,23 +1,23 @@
 defmodule BananaBank.Accounts.Account do
-	use Ecto.Schema
-	import Ecto.Changeset
+  use Ecto.Schema
+  import Ecto.Changeset
 
-	alias BananaBank.Users.User
+  alias BananaBank.Users.User
 
-	@required_params [:balance, :user_id]
+  @required_params [:balance, :user_id]
 
-	schema "accounts" do
-		field :balance, :decimal
-		belongs_to :user, User
+  schema "accounts" do
+    field :balance, :decimal
+    belongs_to :user, User
 
-		timestamps()
-	end
+    timestamps()
+  end
 
-	def changeset(account \\ %__MODULE__{}, params) do
-		account
-		|> cast(params, @required_params)
-		|> validate_required(@required_params)
-		|> check_constraint(:balance, name: :balance_must_be_positive)
+  def changeset(account \\ %__MODULE__{}, params) do
+    account
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> check_constraint(:balance, name: :balance_must_be_positive)
     |> unique_constraint(:user_id, name: :accounts_user_id_unique)
-	end
+  end
 end
--- lib/banana_bank/accounts/transaction.ex
+++ lib/banana_bank/accounts/transaction.ex
@@ -6,25 +6,29 @@ defmodule BananaBank.Accounts.Transaction do
 
   def call(from_account_id, to_account_id, value) do
     with %Account{} = from_account <- Repo.get(Account, from_account_id),
-         %Account{} = to_account <- Repo.get(Account, to_account_id) do
+         %Account{} = to_account <- Repo.get(Account, to_account_id),
+         {:ok, value} <- Decimal.cast(value) do
       Multi.new()
-      |> withdraw(to_account, value)
-      |> deposit(from_account, value)
+      |> withdraw(from_account, value)
+      |> deposit(to_account, value)
       |> Repo.transaction()
     else
       nil -> {:error, :not_found}
+      :error -> {:error, "Invalid value"}
     end
   end
 
-  defp deposit(multi, from_account, value ) do
-    new_balance = Decimal.add(from_account.balance, value)
-    changeset = Account.changeset(from_account, %{balance: new_balance})
-    Multi.update(multi, :deposit, changeset)
-  end
-
-  defp withdraw(multi, to_account, value ) do
+  # Função para o saque.
+  defp withdraw(multi, to_account, value) do
     new_balance = Decimal.sub(to_account.balance, value)
     changeset = Account.changeset(to_account, %{balance: new_balance})
     Multi.update(multi, :withdraw, changeset)
+  end
+
+  # Função para o depósito.
+  defp deposit(multi, from_account, value) do
+    new_balance = Decimal.add(from_account.balance, value)
+    changeset = Account.changeset(from_account, %{balance: new_balance})
+    Multi.update(multi, :deposit, changeset)
   end
 end
